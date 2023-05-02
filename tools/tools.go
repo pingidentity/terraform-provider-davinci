e996e02306d8d2ac55d5ac9fdf70a02a1c13fb6a
Samir Gandhi <17574913+samir-gandhi@users.noreply.github.com>
2023-05-02 13:26:17 -0600

    plugin for davinci_connection docs generation (#116)

--- tools/tools.go
+++ tools/tools.go
@@ -18,5 +18,6 @@ import (
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 	_ "github.com/katbyte/terrafmt"
 	_ "github.com/pavius/impi/cmd/impi"
+	_ "github.com/samir-gandhi/dvgenerate/cmd/generate"
 	_ "github.com/terraform-linters/tflint"
 )
