49459410371a6255ef029753958a909301e860d5
Patrick Cowland <patrickcowland@pingidentity.com>
2023-08-24 16:18:41 +0100

    `resource/davinci_connection`: Fixed error when attempting to import resource state.

--- internal/service/davinci/resource_connection.go
+++ internal/service/davinci/resource_connection.go
@@ -3,12 +3,14 @@ package davinci
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/pingidentity/terraform-provider-davinci/internal/sdk"
+	"github.com/pingidentity/terraform-provider-davinci/internal/utils"
 	dv "github.com/samir-gandhi/davinci-client-go/davinci"
 )
 
@@ -74,7 +76,7 @@ func ResourceConnection() *schema.Resource {
 			},
 		},
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			StateContext: resourceConnectionImport,
 		},
 	}
 }
@@ -264,6 +266,33 @@ func resourceConnectionDelete(ctx context.Context, d *schema.ResourceData, meta 
 	d.SetId("")
 
 	return diags
+}
+
+func resourceConnectionImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+
+	idComponents := []utils.ImportComponent{
+		{
+			Label:  "environment_id",
+			Regexp: regexp.MustCompile(`[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}`),
+		},
+		{
+			Label:     "davinci_connection_id",
+			Regexp:    regexp.MustCompile(`[a-f0-9]{32}`),
+			PrimaryID: true,
+		},
+	}
+
+	attributes, err := utils.ParseImportID(d.Id(), idComponents...)
+	if err != nil {
+		return nil, err
+	}
+
+	d.Set("environment_id", attributes["environment_id"])
+	d.SetId(attributes["davinci_connection_id"])
+
+	resourceConnectionRead(ctx, d, meta)
+
+	return []*schema.ResourceData{d}, nil
 }
 
 func flattenConnectionProperties(connectionProperties *dv.Properties) ([]map[string]interface{}, error) {
