bf6325754e9197880babe81fb891e726417691a3
Patrick Cowland <patrickcowland@pingidentity.com>
2023-12-29 12:53:27 +0000

    Fix `davinci_application_flow_policy` import example doc

--- examples/resources/davinci_application_flow_policy/import.sh
+++ examples/resources/davinci_application_flow_policy/import.sh
@@ -1,1 +1,1 @@
-$ terraform import davinci_application.example <environment_id>/<davinci_application_id>/<davinci_application_flow_policy_id>
+$ terraform import davinci_application_flow_policy.example <environment_id>/<davinci_application_id>/<davinci_application_flow_policy_id>
